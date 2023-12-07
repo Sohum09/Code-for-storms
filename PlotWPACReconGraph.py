c77d041ec645763af6a5eb047cd7be750a8bfe3a
Sohum09 <64685057+Sohum09@users.noreply.github.com>
2023-12-07 19:07:50 +0530

    Update PlotWPACReconGraph.py

--- PlotWPACReconGraph.py
+++ PlotWPACReconGraph.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
 
-# Inputting the data
-# Format" ["Abbreviated month/Day/Year/Time", 700mb Height(m), Dropsonde(mb)]
+# Format: ["Abbreviated Month/Day/Year/UTC", 700mb Height in metres, Observed pressure] 
+# Use None if data is not available for that particular point.
 data = [
     ["Jun 17 1981 0619", None, 995],
     ["Jun 18 1981 0047", 3029, None],
@@ -18,18 +18,28 @@ data = [
     ["Jun 20 1981 0348", 2787, 963],
 ]
 
-# Extracting data for plotting
+# Extracting data for plotting:
 dates = [datetime.strptime(row[0], "%b %d %Y %H%M") for row in data]
 observed_pressure = [row[2] for row in data]
 converted_pressure = [645 + 0.115 * row[1] if row[1] is not None else None for row in data]
 
-# Plotting
+# Plotting:
 plt.figure(figsize=(10, 6))
-plt.plot(dates, observed_pressure, marker='o', linestyle='-', color='blue', label='Observed Pressure')
-plt.plot(dates, converted_pressure, marker='s', linestyle='--', color='orange', label='Converted Pressure from 700mb Height')
+observed_line, = plt.plot(dates, observed_pressure, marker='o', linestyle='-', color='blue', label='Observed Pressure')
+converted_line, = plt.plot(dates, converted_pressure, marker='s', linestyle='--', color='orange', label='Converted Pressure from 700mb Height')
+
+# Adding text on plot for the datapoints:
+for i, txt in enumerate(observed_pressure):
+    if txt is not None:
+        plt.annotate(str(txt), (dates[i], txt), textcoords="offset points", xytext=(0,-15), ha='center', color=observed_line.get_color())
+
+for i, txt in enumerate(converted_pressure):
+    if txt is not None:
+        plt.annotate(str(round(txt, 2)), (dates[i], txt), textcoords="offset points", xytext=(0,10), ha='center', color=converted_line.get_color())
+
 plt.xlabel('Date+Time (UTC)')
 plt.ylabel('Pressure (mb)')
-plt.title('Synoptic fixes of 05W TY June 1981')
+plt.title('Recon data for 05W TY June 1981')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
