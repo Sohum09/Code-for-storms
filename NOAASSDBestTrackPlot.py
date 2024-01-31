d285fd7af568faff8299f91443013d6749e6a6ab
Sohum09 <64685057+Sohum09@users.noreply.github.com>
2024-01-31 17:28:20 +0530

    Update NOAASSDBestTrackPlot.py

--- NOAASSDBestTrackPlot.py
+++ NOAASSDBestTrackPlot.py
@@ -29,7 +29,38 @@ parsed_data = parse_data(btk_data)
 
 lines = parsed_data.split('\n')
 
-fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(12, 10))
+cdx, cdy, winds, status, timeCheck, pres, DateTime, r34 = [], [], [], [], [], [], [], []
+stormName = ""
+
+for line in lines:
+    if line.strip():
+        parameters = line.split(',')
+        if parameters[6][-1] == 'S':
+            cdy.append((float(parameters[6][:-1].strip()) / 10)*-1)
+        else:
+            cdy.append(float(parameters[6][:-1].strip()) / 10)
+    
+        if parameters[7][-1] == 'W':
+            cdx.append((float(parameters[7][:-1].strip()) / 10)*-1)
+        else:
+            cdx.append(float(parameters[7][:-1].strip()) / 10)
+
+        winds.append(int(parameters[8].strip()))
+        status.append(parameters[10].strip())
+        timeCheck.append((parameters[2][-2:].strip()))
+        date = parameters[2].strip()
+        date = f'{date[:4]}-{date[4:6]}-{date[6:8]} {timeCheck[-1]}:00:00'
+        DateTime.append(date)
+        pres.append(int(parameters[9].strip()))
+        r34.append(int(parameters[11].strip()))
+        stormName = parameters[27].strip()
+
+#-------------------------------DEBUG INFORMATION-----------------------------------
+print(cdx, "\n", cdy, "\n", winds, "\n", status, "\n", timeCheck, "\n")
+#-----------------------------------------------------------------------------------
+
+#Beginning work on the actual plotting of the data:
+fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree(central_longitude=180)}, figsize=(12, 10))
 
 ax.add_feature(cfeature.COASTLINE, linewidth=0.5)
 ax.add_feature(cfeature.BORDERS, linewidth=0.5)
@@ -38,86 +69,63 @@ ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
 
 ax.gridlines(draw_labels=True, linewidth=0.5, linestyle='--', color='gray')
 maxLat, minLat, maxLong, minLong = -999, 999, -999, 999
-vmax = 0
+vmax, statMaxIndx = 0, 0
 
-bt_list = []
-cdx, cdy = [], []
-
-for line in lines:
-  if line.strip():
-    params = line.split(',')
-    for i in range(len(params)):
-      bt_list.append(params[i].strip())
-    coord_y = float(bt_list[6][:-2] + "." + bt_list[6][-2:-1])
-    coord_x = float(bt_list[7][:-2] + "." + bt_list[7][-2:-1])
+#Plotting the markers...
+for i in range(0, len(cdx)):
+    if cdx[i] == ' ' or cdy[i] == ' ' or winds[i] == ' ':
+       continue
     
-    if(params[6][-1] == 'S'):
-      coord_y *= -1
-    if(params[7][-1] == 'W'):
-      coord_x *= -1
-    cdx.append(coord_x)
-    cdy.append(coord_y)
+    coord_x, coord_y = float(cdx[i])+180 if float(cdx[i])<0 else float(cdx[i])-180, float(cdy[i])
+    wind = int(winds[i])
 
+    #Setup for finding bounding box...
     if(maxLat < coord_y):
-      maxLat = coord_y
+        maxLat = coord_y
     if(minLat > coord_y):
-      minLat = coord_y
+        minLat = coord_y
     if(maxLong < coord_x):
-      maxLong = coord_x
+        maxLong = coord_x
     if(minLong > coord_x):
-      minLong = coord_x 
+        minLong = coord_x 
+    
+    #Setup for displaying VMAX as well as peak Status...
+    if vmax < wind:
+        vmax = wind
+        statMaxIndx = i
 
-    if vmax < int(bt_list[8]):
-      vmax = int(bt_list[8])
+    if(int(timeCheck[i]) % 6  == 0):
+        #Mark scatter plots...
+        if status[i] in ['DB', 'WV', 'LO', 'MD']:
+            plt.scatter(coord_x, coord_y, color='#444764', marker='^')
+        elif status[i] == 'EX':
+            if int(wind) >= 64:
+                plt.scatter(coord_x, coord_y, color='#ffff00', marker='^')
+            elif int(wind) >= 34:
+                plt.scatter(coord_x, coord_y, color='g', marker='^')
+            else:
+                plt.scatter(coord_x, coord_y, color='b', marker='^')
+        elif status[i] == 'SS':
+            plt.scatter(coord_x, coord_y, color='g', marker='s')
+        elif status[i] == 'SD':
+            plt.scatter(coord_x, coord_y, color='b', marker='s')
+        else:
+            if int(wind) >= 137:
+                plt.scatter(coord_x, coord_y, color='m', marker='o')
+            elif int(wind) >= 112:
+                plt.scatter(coord_x, coord_y, color='r', marker='o')
+            elif int(wind) >= 96:
+                plt.scatter(coord_x, coord_y, color='#ff5908', marker='o')
+            elif int(wind) >= 81:
+                plt.scatter(coord_x, coord_y, color='#ffa001', marker='o')
+            elif int(wind) >= 64:
+                plt.scatter(coord_x, coord_y, color='#ffff00', marker='o')
+            elif int(wind) >= 34:
+                plt.scatter(coord_x, coord_y, color='g', marker='o')
+            else:
+                plt.scatter(coord_x, coord_y, color='b', marker='o')
 
-    bt_list = []
-
-plt.plot(cdx, cdy, color="k", linestyle="--", label='TC Path')
-
-for line in lines:
-  if line.strip():
-    para = line.split(',')
-    bt_list = []
-    for i in range(len(para)):
-      bt_list.append(para[i].strip())
-
-    coord_y = float(bt_list[6][:-2] + "." + bt_list[6][-2:-1])
-    coord_x = float(bt_list[7][:-2] + "." + bt_list[7][-2:-1])
-
-    if(params[6][-1] == 'S'):
-      coord_y *= -1
-    if(params[7][-1] == 'W'):
-      coord_x *= -1
-
-    if bt_list[10] in ['DB', 'WV', 'LO', 'MD']:
-      plt.scatter(coord_x, coord_y, color='#444764', marker='^')
-    elif bt_list[10] == 'EX':
-      if int(bt_list[8]) >= 64:
-        plt.scatter(coord_x, coord_y, color='#ffff00', marker='^')
-      elif int(bt_list[8]) >= 34:
-        plt.scatter(coord_x, coord_y, color='g', marker='^')
-      else:
-        plt.scatter(coord_x, coord_y, color='b', marker='^')
-    elif bt_list[10] == 'SS':
-      plt.scatter(coord_x, coord_y, color='g', marker='s')
-    elif bt_list[10] == 'SD':
-      plt.scatter(coord_x, coord_y, color='b', marker='s')
-    else:
-      if int(bt_list[8]) >= 137:
-        plt.scatter(coord_x, coord_y, color='m', marker='o')
-      elif int(bt_list[8]) >= 112:
-        plt.scatter(coord_x, coord_y, color='r', marker='o')
-      elif int(bt_list[8]) >= 96:
-        plt.scatter(coord_x, coord_y, color='#ff5908', marker='o')
-      elif int(bt_list[8]) >= 81:
-        plt.scatter(coord_x, coord_y, color='#ffa001', marker='o')
-      elif int(bt_list[8]) >= 64:
-        plt.scatter(coord_x, coord_y, color='#ffff00', marker='o')
-      elif int(bt_list[8]) >= 34:
-        plt.scatter(coord_x, coord_y, color='g', marker='o')
-      else:
-        plt.scatter(coord_x, coord_y, color='b', marker='o')
-
+#Setting the coordinates for the bounding box...
 center_x = (minLong + maxLong)/2
 center_y = (minLat + maxLat)/2
 
@@ -127,15 +135,16 @@ center_height = abs(maxLat - minLat)
 ratio = (center_height/center_width)
 print(ratio)
 if ratio < 0.3:
-  ax.set_xlim(center_x-center_width, center_x+center_width)
-  ax.set_ylim(center_y-(center_width/2), center_y+(center_width/2))
-elif ratio>0.7:
-  ax.set_xlim(center_x-(center_height), center_x+(center_height))
-  ax.set_ylim(center_y-center_height, center_y+center_height)
+    ax.set_xlim(center_x-center_width, center_x+center_width)
+    ax.set_ylim(center_y-(center_width/2), center_y+(center_width/2))
+elif ratio > 0.7:
+    ax.set_xlim(center_x-(center_height), center_x+(center_height))
+    ax.set_ylim(center_y-center_height, center_y+center_height)
 else:
-  ax.set_xlim(center_x-center_width, center_x+center_width)
-  ax.set_ylim(center_y-center_height, center_y+center_height)
+    ax.set_xlim(center_x-center_width, center_x+center_width)
+    ax.set_ylim(center_y-center_height, center_y+center_height)
 
+#Defining the legend box for the plot...
 legend_elements = [
                   Line2D([0], [0], marker='^', color='w', label='Non-Tropical',markerfacecolor='#444764', markersize=10),
                   Line2D([0], [0], marker='s', color='w', label='Sub-Tropical',markerfacecolor='#444764', markersize=10),
@@ -148,21 +157,38 @@ legend_elements = [
                   Line2D([0], [0], marker='o', color='w', label='Category 5',markerfacecolor='m', markersize=10),
 ]
 
-def calc_ACE(btkLines):
+#Building the function that calculates ACE...
+def calc_ACE(winds, timeCheck):
     ace = 0
-    for line in btkLines:
-        if line.strip():
-            params = line.split(',')
-            if(params[11].strip() == '34' and params[10].strip() not in ['DB', 'LO', 'EX', 'WV']):
-                ace += (int(params[8]) ** 2) / 10000
+   
+    for i in range(len(winds)-1):
+        if(winds[i] == ' '):
+            continue
+        time = int(timeCheck[i]) % 6 #If it is synoptic time and meets...
+        if(time==0 and r34[i] == 34 and status[i] not in ['DB', 'LO', 'WV', 'EX']): 
+           ace += (int(winds[i]) ** 2) / 10000
     return "{:.4f}".format(ace)
 
-# Add labels and legend
+#Plotting the TC Path...
+LineX = []
+LineY = []
+
+for i in range(len(cdx)):
+    if cdx[i] == ' ' or cdy[i] == ' ':
+        continue
+    LineX.append(float(cdx[i])+180 if float(cdx[i])<0 else float(cdx[i])-180)
+    LineY.append(float(cdy[i]))
+
+plt.plot(LineX, LineY, color="k", linestyle="--")
+plt.text(LineX[0], LineY[0]+0.5, f'{DateTime[0]}')
+plt.text(LineX[len(LineX)-1], LineY[len(LineX)-1]+0.5, f'{DateTime[len(LineX)-1]}')
+
+#Applying final touches...
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
-plt.title(f'Cyclone Best Track Test: {btkID}{yr}')
+plt.title(f'{btkID.upper()}{yr} {stormName}')
 plt.title(f'VMAX: {vmax} Kts', loc='left', fontsize=9)
-plt.title(f'ACE: {calc_ACE(lines)}', loc='right', fontsize=9)
-ax.legend(handles=legend_elements, loc='upper right' if btkID[:2]=="sh" or btkID[:2]=="ep" else "upper left")
+plt.title(f'ACE: {calc_ACE(winds, timeCheck)}', loc='right', fontsize=9)
+ax.legend(handles=legend_elements, loc='upper left')
 plt.grid(True)
 plt.show()
