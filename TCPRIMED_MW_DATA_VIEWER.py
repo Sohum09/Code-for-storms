a53608806f6c747dfcc6d4ce867ebcf6a6e9bb92
Sohum09 <64685057+Sohum09@users.noreply.github.com>
2024-08-10 23:42:06 +0530

    Update TCPRIMED_MW_DATA_VIEWER.py

--- TCPRIMED_MW_DATA_VIEWER.py
+++ TCPRIMED_MW_DATA_VIEWER.py
@@ -1,5 +1,7 @@
-#URL: Enter here as per Step 3! 
-url = "https://noaa-nesdis-tcprimed-pds.s3.amazonaws.com/v01r00/final/2004/WP/09/TCPRIMED_v01r00-final_WP092004_TMI_TRMM_037529_20040615194915.nc"
+#URL: Enter here as per Step 3!
+url = "https://noaa-nesdis-tcprimed-pds.s3.amazonaws.com/v01r00/final/2004/EP/13/TCPRIMED_v01r00-final_EP132004_AMSRE_AQUA_012577_20040913203813.nc"
+#Enter 1 or 0 if you want to smoothen the output:
+smooth = 0
 
 import subprocess, sys, datetime, requests
 packages = ["netCDF4", "matplotlib", "cartopy"]
@@ -12,18 +14,36 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-
+from scipy.ndimage import gaussian_filter
+from matplotlib.tri import Triangulation, LinearTriInterpolator, CubicTriInterpolator
+import numpy as np
 plt.rcParams['pcolor.shading'] = 'auto'
 
 url_response = requests.get(url)
 FILE_NAME = "Output.nc"
 DS = Dataset(FILE_NAME, memory=url_response.content)
 
+#----------------------------------If using the Appendix, Paste in these bounds----------------------------------------------
 #Select the variables per Step 4 and 5!
-Strm_latitude = DS["passive_microwave/S3/latitude"][:]     # Latitude of observations on swath
-Strm_longitude = DS["passive_microwave/S3/longitude"][:]   # Longitude of observations on swath
-Strm_89H = DS["passive_microwave/S3/TB_85.5H"][:]    # Swath number with instrument variable name and type    
+Strm_latitude = DS["passive_microwave/S6/latitude"][:]
+Strm_longitude = DS["passive_microwave/S6/longitude"][:]
+Strm = DS["passive_microwave/S6/TB_B89.0H"][:]
+#----------------------------------------------------------------------------------------------------------------------------
 color = "89" #Input either "89" or "37" depending on your color scale.
+
+
+if smooth == 1:
+  Strm_latitude_flat = Strm_latitude.flatten()
+  Strm_longitude_flat = Strm_longitude.flatten()
+  Strm_flat = Strm.flatten()
+
+  triang = Triangulation(Strm_longitude_flat, Strm_latitude_flat)
+
+  interpolator = LinearTriInterpolator(triang, Strm_flat)
+  xi, yi = np.meshgrid(np.linspace(Strm_longitude_flat.min(), Strm_longitude_flat.max(), 500), np.linspace(Strm_latitude_flat.min(), Strm_latitude_flat.max(), 500))
+  zi = interpolator(xi, yi)
+
+  zi_smoothed = gaussian_filter(zi, sigma=1)
 
 def mw89():
   newcmp = LinearSegmentedColormap.from_list("", [
@@ -80,8 +100,12 @@ time = DS["overpass_metadata/time"][:].item()
 dt = datetime.datetime.utcfromtimestamp(time)
 formatted_time = dt.strftime('%d/%m %H:%M UTC')
 
+Strm_longitude = (Strm_longitude + 180) % 360 - 180
 col, vm, vn = mw89() if color == "89" else mw37()
-plt.pcolormesh(Strm_longitude, Strm_latitude, Strm_89H, cmap=col, vmax=vm, vmin=vn)
+if smooth == 0:
+  plt.pcolormesh(Strm_longitude, Strm_latitude, Strm, cmap=col, vmax=vm, vmin=vn)
+else:
+  plt.pcolormesh(xi, yi, zi_smoothed, cmap=col, vmax=vm, vmin=vn)
 plt.colorbar(label=f"Brightness Temperature (Kelvin)")
 
 plt.title(f"{name} {formatted_time}\n{instrument_name} ~{color} GHz")
@@ -91,7 +115,9 @@ plt.grid(color='gray', linestyle=':')
 plt.axis("scaled")
 storm_latitude = DS["overpass_storm_metadata/storm_latitude"][:][0]
 storm_longitude = DS["overpass_storm_metadata/storm_longitude"][:][0]
+storm_longitude = (storm_longitude + 180)% 360 - 180
 plt.xlabel("Longitude (degrees east)")
 plt.ylabel("Latitude (degrees north)")
 plt.xlim(storm_longitude - 5, storm_longitude + 5)
 plt.ylim(storm_latitude - 5, storm_latitude + 5)
+print("Longitude range after conversion:", Strm_longitude.min(), Strm_longitude.max())
